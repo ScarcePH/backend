ef2e0ce1ff3687b5d69076addb969bd47ad3e255
M4rxDev <marionrosete1@gmail.com>
2025-12-05 23:01:44 +0800

    [FIX]: gcash attachment should not handover to admin

--- bot/webhook_handler.py
+++ bot/webhook_handler.py
@@ -1,5 +1,5 @@
 from flask import request
-from bot.state.manager import set_handover, is_in_handover
+from bot.state.manager import set_handover, is_in_handover,get_state
 from bot.utils.redis_client import redis_client
 import os
 
@@ -39,7 +39,7 @@ def webhook():
             is_echo = event.get("message", {}).get("is_echo")
             app_id = str(event.get("message", {}).get("app_id"))
             
-            if is_echo and app_id != BOT_APP_ID and not "message" in event:
+            if is_echo and app_id != BOT_APP_ID and not "attachments" in event:
                 user_psid = event["recipient"]["id"]
                 print(f"[ECHO] ADMIN REPLIES THE CHAT")
                 set_handover(user_psid)
@@ -56,16 +56,17 @@ def webhook():
                         print("BOT REPLIED")
                         return "ok", 200
                     
+                    state = get_state(sender_id)
+                    current = state.get("state", "idle")
+                    if current == 'handle_verify_payment':
+                        if "attachments" in msg:
+                            for attachment in msg["attachments"]:
+                                if attachment["type"] == "image":
+                                    image_url = attachment["payload"]["url"]
+                                    print(f"[IMAGE_RECEIVED] {image_url}")
+                                    handle_message(sender_id, image_url)
 
-
-                    if "attachments" in msg:
-                        for attachment in msg["attachments"]:
-                            if attachment["type"] == "image":
-                                image_url = attachment["payload"]["url"]
-                                print(f"[IMAGE_RECEIVED] {image_url}")
-                                handle_message(sender_id, image_url)
-
-                        return "ok", 200
+                            return "ok", 200
 
 
     return "ok", 200
