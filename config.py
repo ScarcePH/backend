0240343a6b25852d819bf11f30266e99f0e6dbf2
M4rxDev <marionrosete1@gmail.com>
2025-12-07 23:24:41 +0800

    prod db uri

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/scarce-db"
+    ## LOCAL
+    # SQLALCHEMY_DATABASE_URI = "postgresql://localhost/scarce-db"
+    ##PROD
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
