648d9d32694eecde7780886c55d71dfc7136d52a
M4rxDev <marionrosete1@gmail.com>
2025-12-11 22:52:02 +0800

    check logs

--- bot/services/messenger.py
+++ bot/services/messenger.py
@@ -2,11 +2,13 @@ from bot.services.send_text import send_text_message, send_template_message
 from bot.core.constants import BOT_TAG, QUICK_REPLIES
 
 def reply(sender_id, message, quick_replies=QUICK_REPLIES):
+    print("SENDING TEXT")
     send_text_message(sender_id, f"{message}\n{BOT_TAG}", quick_replies)
 
 
 
 def send_carousel(sender_id, products=None):
+    print("SENDING CAROUSEL")
     items = []
     for item in products:
         carousel={
