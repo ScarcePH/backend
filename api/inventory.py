475a380fa051ee8534bce317e57c86e52541df76
M4rxDev <marionrosete1@gmail.com>
2025-12-11 09:44:29 +0800

    [OPTIMIZE]: db query based instead of loading all items and filter

--- api/inventory.py
+++ api/inventory.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from db.database import db
 from db.models.inventory import Inventory
-from db.repository.inventory import get_all_inventory,save_inventory
+from db.repository.inventory import get_all_inventory,save_inventory,search_items,get_item_sizes
 
 inventory_bp = Blueprint("inventory", __name__)
 
@@ -14,3 +14,25 @@ def create_inventory():
 @inventory_bp.route("/get-all-inventory", methods=["GET"])
 def fetch_inventory():
     return get_all_inventory()
+
+@inventory_bp.route("/get-inventory-name-size", methods=["GET"])
+def fetch_inventory_name_size():
+    name = request.args.get('name') 
+    size = request.args.get('size')
+
+    if not name:
+        return {"message": "Name is required"}, 400
+    
+  
+    result = search_items(name, size)  
+    
+    return result
+
+@inventory_bp.route("/get-inventory-by-size", methods=["GET"])
+def fetch_item_by_size():
+    name = request.args.get('name') 
+    size = request.args.get('size') 
+    if not size:
+        return {"message": "Size is required"}, 400
+    
+    return get_item_sizes(size,name)
--- db/repository/inventory.py
+++ db/repository/inventory.py
@@ -1,5 +1,6 @@
 from db.models import Inventory
 from db.database import db
+import re
 
 
 def save_inventory(data: dict):
@@ -23,3 +24,59 @@ def get_all_inventory():
         }
         for item in items
     ]
+
+def search_items(name, size=None):
+
+    query = Inventory.query.filter(
+        Inventory.name.ilike(f"%{name}%")
+    )
+
+    if size:
+        query = query.filter(Inventory.size.ilike(f"%{size}%"))
+
+    # Return ALL matches
+    items = query.all()
+
+    if not items:
+        return {"found": False, "reason": "not_found"}
+
+    results = []
+    for item in items:
+        results.append({
+            "id": item.id,
+            "name": item.name,
+            "size": item.size,
+            "price": item.price,
+            "url": item.url
+        })
+
+    return {
+        "found": True,
+        "count": len(results),
+        "items": results
+    }
+
+def extract_size(query):
+    match = re.search(r'\b\d+(\.\d+)?\b', query)
+    if match:
+        return match.group(0)
+    return None
+
+
+def get_item_sizes(size, item_name):
+    # Get DB session
+   
+
+    query = Inventory.query.filter(Inventory.size.ilike(f"%{size}%"))
+
+    if not query:
+        return []
+
+    available_items = [x.name for x in query]
+
+    formatted_list = ", ".join(f"'{name}'" for name in available_items)
+
+    return (
+        f"We don't have '{item_name}' in size {size}us. "
+        f"However, these are available in size {size}us: {formatted_list}."
+    )
