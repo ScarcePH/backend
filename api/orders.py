118b69ba5827b1b57b2a5ccd472d56391fba31c6
M4rxDev <marionrosete1@gmail.com>
2025-12-23 21:02:34 +0800

    use model dict util

--- api/orders.py
+++ api/orders.py
@@ -2,31 +2,32 @@ from flask import Blueprint, request, jsonify
 from db.database import db
 from db.models import Order
 from db.repository.order import save_order, get_order
+from middleware.admin_required import admin_required
 
 orders_bp = Blueprint("orders", __name__)
 
 @orders_bp.route("/save-order", methods=["POST"])
+@admin_required(allowed_roles=["super_admin"])
 def create_order():
     data = request.json
     save_order(data)
     return jsonify({"status": "ok", "order": data})
 
 @orders_bp.route("get-order", methods=["GET"])
+@admin_required(allowed_roles=["super_admin"])
 def get_order_by_senderid():
     sender_id= request.args.get('sender_id')
     order = get_order(sender_id)
     return order
 
 @orders_bp.route("/orders/<int:order_id>", methods=["PUT"])
+@admin_required(allowed_roles=["super_admin"])
 def update_order(order_id):
     order = Order.query.get_or_404(order_id)
     data = request.json or {}
 
     # Allowed fields
     editable_fields = {
-        "payment_method",
-        "payment_reference",
-        "payment_verified",
         "status"
     }
 
@@ -34,25 +35,13 @@ def update_order(order_id):
         if field in editable_fields:
             setattr(order, field, value)
 
-    # BUSINESS LOGIC: inventory status must follow order status
-    if "status" in data:
-        inv = order.inventory_item
-
-        if data["status"] == "pending":
-            inv.status = "reserved"
-
-        elif data["status"] == "confirmed":
-            inv.status = "sold"
-
-        elif data["status"] == "cancelled":
-            inv.status = "available"
-
     db.session.commit()
 
     return jsonify({"message": "Order updated", "order_id": order.id})
 
 
 @orders_bp.route("/orders/<int:order_id>", methods=["DELETE"])
+@admin_required(allowed_roles=["super_admin"])
 def delete_order(order_id):
     order = Order.query.get_or_404(order_id)
 
--- db/repository/order.py
+++ db/repository/order.py
@@ -16,48 +16,51 @@ def get_order(sender_id):
         .filter(Customers.sender_id == sender_id)
         .all()
     )
-    result = []
-    for order in orders:
-        result.append({
-            "order_id": order.id,
-            "status": order.status,
-            "created_at": order.created_at,
-            "updated_at": order.updated_at,
-            "customer": {
-                "id": order.customer.id,
-                "name": order.customer.name,
-                "phone": order.customer.phone,
-                "address": order.customer.address,
-            } if order.customer else None,
-            "inventory": {
-                "id": order.inventory.id,
-                "name": order.inventory.name,
-                "description": order.inventory.description,
-            } if order.inventory else None,
-            "variation": {
-                "id": order.variation.id,
-                "size": order.variation.size,
-                "condition": order.variation.condition,
-                "price": float(order.variation.price),  # convert Decimal to float if needed
-                "stock": order.variation.stock,
-                "url": order.variation.url,
-                "image": order.variation.image,
-                "status": order.variation.status,
-            } if order.variation else None,
-            "payment": {
-                "id": order.payment.id,
-                "payment_method": order.payment.payment_method,
-                "to_settle": order.payment.total_amount - order.payment.received_amount,
-            } if order.payment else None,
-            "shipment": {
-                "id": order.shipment.id,
-                "carrier": order.shipment.carrier,
-                "tracking": order.shipment.tracking,
-                "status": order.shipment.status,
-            } if order.shipment else None,
+    result = [Order.to_dict(order) for order in orders]
+    # result = []
+    # for order in orders:
+    #     result.append({
+    #         "order_id": order.id,
+    #         "status": order.status,
+    #         "created_at": order.created_at,
+    #         "updated_at": order.updated_at,
+    #         "customer": {
+    #             "id": order.customer.id,
+    #             "name": order.customer.name,
+    #             "phone": order.customer.phone,
+    #             "address": order.customer.address,
+    #         } if order.customer else None,
+    #         "inventory": {
+    #             "id": order.inventory.id,
+    #             "name": order.inventory.name,
+    #             "description": order.inventory.description,
+    #         } if order.inventory else None,
+    #         "variation": {
+    #             "id": order.variation.id,
+    #             "size": order.variation.size,
+    #             "condition": order.variation.condition,
+    #             "price": float(order.variation.price),  # convert Decimal to float if needed
+    #             "stock": order.variation.stock,
+    #             "url": order.variation.url,
+    #             "image": order.variation.image,
+    #             "status": order.variation.status,
+    #         } if order.variation else None,
+    #         "payment": {
+    #             "id": order.payment.id,
+    #             "payment_method": order.payment.payment_method,
+    #             "to_settle": order.payment.total_amount - order.payment.received_amount,
+    #         } if order.payment else None,
+    #         "shipment": {
+    #             "id": order.shipment.id,
+    #             "carrier": order.shipment.carrier,
+    #             "tracking": order.shipment.tracking,
+    #             "status": order.shipment.status,
+    #         } if order.shipment else None,
     
-        })
+    #     })
     return result
+
+
 
     
 
