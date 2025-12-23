195fc07431698a7b7bc8d2c3bffaba8fae4776c6
M4rxDev <marionrosete1@gmail.com>
2025-12-23 21:09:28 +0800

    add missing key

--- db/models/payment.py
+++ db/models/payment.py
@@ -28,4 +28,5 @@ class Payment(db.Model):
             "order_id": self.order_id,
             "created_at": self.created_at.isoformat() if self.created_at else None,
             "updated_at": self.updated_at.isoformat() if self.updated_at else None,
+            "to_settle": self.total_amount - self.received_amount,
         }
