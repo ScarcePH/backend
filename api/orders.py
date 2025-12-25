6e9ea63e0563aeb4dfcc71184675763d0a082f3e
M4rxDev <marionrosete1@gmail.com>
2025-12-25 17:08:49 +0800

    get all pending order & update order

--- api/orders.py
+++ api/orders.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from db.database import db
 from db.models import Order
-from db.repository.order import save_order, get_order
+from db.repository.order import save_order, get_order, get_all_pending_orders, update_order
 from middleware.admin_required import admin_required
 
 orders_bp = Blueprint("orders", __name__)
@@ -20,37 +20,19 @@ def get_order_by_senderid():
     order = get_order(sender_id)
     return order
 
-@orders_bp.route("/orders/<int:order_id>", methods=["PUT"])
+@orders_bp.route("orders/get-all-pending", methods=["GET"])
 @admin_required(allowed_roles=["super_admin"])
-def update_order(order_id):
-    order = Order.query.get_or_404(order_id)
-    data = request.json or {}
+def pending_orders():
+    pending_orders = get_all_pending_orders()
+    return pending_orders
 
-    # Allowed fields
-    editable_fields = {
-        "status"
-    }
-
-    for field, value in data.items():
-        if field in editable_fields:
-            setattr(order, field, value)
-
-    db.session.commit()
-
-    return jsonify({"message": "Order updated", "order_id": order.id})
-
-
-@orders_bp.route("/orders/<int:order_id>", methods=["DELETE"])
+@orders_bp.route("orders/update-status", methods=["POST"])
 @admin_required(allowed_roles=["super_admin"])
-def delete_order(order_id):
-    order = Order.query.get_or_404(order_id)
+def update():
+    data = request.json
+    status = data.get("status")
+    order_id = data.get("order_id")
+    order = update_order(order_id, status)
+    return order
 
-    # Free up inventory if needed
-    inv = order.inventory_item
-    if inv.status in ["reserved", "sold"]:
-        inv.status = "available"
 
-    db.session.delete(order)
-    db.session.commit()
-
-    return jsonify({"message": "Order deleted"})
